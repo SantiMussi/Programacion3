7af8413d60094f87db6b87e7d84ad7086af72795
Matias Masucci <masumatias@gmail.com>
2024-09-13 09:13:23 -0300

    Clase2

---
+++ Clase2/Clase2-ACT1/src/arbol.java
@@ -0,0 +1,15 @@
+public class arbol {
+        int value;
+        arbol left, right;
+
+        public arbol(int value) {
+            this.value = value;
+            left = right = null;
+        }
+}
+
+
+
+
+
+
